6c87fa1cb7e3730a2b7c8cc9221b7ddbdaf2fe1d
ericksitocg <ericksitocg@hotmail.com>
2019-06-29 20:16:31 -0500

    Ingreso y almacenamiento del mensaje y el codigo

--- cifradoGreySM.asm
+++ cifradoGreySM.asm
@@ -1,20 +1,62 @@
-	# All program code is placed after the
 	# Directiva de ensamblador .text todas las lineas debajo son codigo
 	.text
 
 	# Declarando la funcion global main
 	.globl	main
 	
-main:
-	#Pruebas Hola mundo de base para iniciar el programa
-	li	$v0,4		# Syscall imprimir un string loadinmediate
-	la	$a0, msg	# Puntero a string (load the address of msg)
+main:	
+	#Presentando para pedir mensaje
+	li $v0,4
+	la $a0,menj_pedir_mensaje
 	syscall
-	li	$v0,10		# Syscall Salir
+
+	#Tomando la entrada del usuario como texto (mensaje)
+	li $v0,8
+	la $a0,buffer
+	li $a1,50
+	syscall
+	
+	#Presentando para pedir codigo
+	li $v0,4
+	la $a0,menj_pedir_codigo
+	syscall
+	
+	#Tomando la entrada del usuario como texto (codigo)
+	li $v0,8
+	la $a0,buffer2
+	li $a1,50
+	syscall
+	
+	#Presentando mensaje informacion_1
+	li $v0,4
+	la $a0,menj_informar_encriptacion1
+	syscall 
+	
+	#Presentado datos finales (mensaje)
+	li $v0,4
+	la $a0,buffer
+	syscall
+	
+	#Presentando mensaje informacion_2
+	li $v0,4
+	la $a0,menj_informar_encriptacion2
+	syscall 
+	
+	#Presentado datos finales (codigo)
+	li $v0,4
+	la $a0,buffer2
+	syscall
+	
+	#terminando el programa
+	li $v0,10		# Syscall Salir
 	syscall
 
 	# .data Directiva ensamblador todas las estructuras de memoria estand declaradas de la directiva
 	.data
 	
-	#La directiva .asciiz crea un string en memoria terminado en caracter null
-msg:	.asciiz	"Pruebas cifrado GreyCM!\n"
+buffer: .space 50
+buffer2: .space 50
+menj_pedir_mensaje:	.asciiz "Ingrese el mensaje a encriptar(max 50 letras): "
+menj_pedir_codigo:	.asciiz "Ingrese el codigo a usar para la encriptacion: "
+menj_informar_encriptacion1:	.asciiz "Su codigo encriptado es: "
+menj_informar_encriptacion2:	.asciiz "Usando el codigo: "
